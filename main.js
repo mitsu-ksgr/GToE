8ab8ee30aa099a16bd7620242575351993af74f2
mitsu-ksgr <mitsu.naka.dev@gmail.com>
2016-11-27 03:56:36 +0900

    Add menu

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //-----------------------------------------------------------------------------
 //  Google Translate on Electron.
 //-----------------------------------------------------------------------------
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const TARGET_URL = 'https://translate.google.com/';
 
-let win;    //! Browser Window object.
+let win = null;    //! Browser Window object.
 function createWindow()
 {
     win = new BrowserWindow({width: 1080, height: 800});
@@ -15,17 +15,75 @@ function createWindow()
     });
 };
 
+function setupMenu()
+{
+    Menu.setApplicationMenu(Menu.buildFromTemplate([
+        {
+            label: "GToE",
+            submenu: [
+                {
+                    label: "About GToE",
+                    selector: "orderFrontStandardAboutPanel:"
+                },
+                { type: "separator" },
+                {
+                    label: "Quit",
+                    accelerator: "Command+Q",
+                    click: function() { app.quit(); }
+                }
+            ]
+        },
+        {
+            label: "Edit",
+            submenu: [
+                {
+                    label: "Undo",
+                    accelerator: "CmdOrCtrl+Z",
+                    selector: "undo:" },
+                {
+                    label: "Redo",
+                    accelerator: "Shift+CmdOrCtrl+Z",
+                    selector: "redo:"
+                },
+                { type: "separator" },
+                {
+                    label: "Cut",
+                    accelerator: "CmdOrCtrl+X",
+                    selector: "cut:"
+                },
+                {
+                    label: "Copy",
+                    accelerator: "CmdOrCtrl+C",
+                    selector: "copy:"
+                },
+                {
+                    label: "Paste",
+                    accelerator: "CmdOrCtrl+V",
+                    selector: "paste:"
+                },
+                {
+                    label: "Select All",
+                    accelerator: "CmdOrCtrl+A",
+                    selector: "selectAll:"
+                }
+            ]
+        }
+    ]));
+}
+
 
 //-----------------------------------------------------------------------------
 //  App Events
 //-----------------------------------------------------------------------------
 app.on('ready', () => {
     createWindow();
+    setupMenu();
 });
 
 app.on('activate', () => {
     if (win === null) {
         createWindow();
+        setupMenu();
     }
 });
 
